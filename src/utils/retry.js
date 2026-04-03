36121917e0a19801d3fc379abab0845c5f41bf2f
Akshay-Anand <dev.renosync@gmail.com>
2026-04-04 04:38:47 +0530

    enhance retry logic to handle rate limiting errors

--- src/utils/retry.js
+++ src/utils/retry.js
@@ -4,9 +4,14 @@ const retry = async (fn, retries = 3) => {
   return pRetry(fn, {
     retries,
     onFailedAttempt: error => {
+      console.log(error);
+      const status = error?.response?.status;
       console.log(
         `Attempt ${error.attemptNumber} failed. ${error.retriesLeft} retries left.`
       );
+      if (status === 429) {
+        throw new pRetry.AbortError("Rate limited");
+      }
     },
   });
 };
